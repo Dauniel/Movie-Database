51090c3cc7780262d8960bde1f8bf3dbd65b160b
Daniel <102876965+Dauniel@users.noreply.github.com>
2024-08-04 15:50:15 -0700

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -8,40 +8,40 @@ Movie::Movie(const string& id, const string& title, const string& release_year,
              const vector<string>& directors, const vector<string>& actors,
              const vector<string>& genres, float rating)
 {
-    // Replace this line with correct code.
+
 }
 
 string Movie::get_id() const
 {
-    return "DUMMY";  // Replace this line with correct code.
+    return "DUMMY";  
 }
 
 string Movie::get_title() const
 {
-    return "DUMMY";  // Replace this line with correct code.
+    return "DUMMY";  
 }
 
 string Movie::get_release_year() const
 {
-    return "DUMMY";  // Replace this line with correct code.
+    return "DUMMY";  
 }
 
 float Movie::get_rating() const
 {
-    return -999;  // Replace this line with correct code.
+    return -999;  
 }
 
 vector<string> Movie::get_directors() const
 {
-    return vector<string>();  // Replace this line with correct code.
+    return vector<string>();  
 }
 
 vector<string> Movie::get_actors() const
 {
-    return vector<string>();  // Replace this line with correct code.
+    return vector<string>();  
 }
 
 vector<string> Movie::get_genres() const
 {
-    return vector<string>();  // Replace this line with correct code.
+    return vector<string>();  
 }
